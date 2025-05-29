e0aaa863e6425db1c5c47b7369a927e774a17bc8
Tomáš Tintěra <Tomas_Tintera@epam.com>
2025-05-29 09:03:36 +0200

    Interface v2

--- MultiCopy.cpp
+++ MultiCopy.cpp
@@ -2,73 +2,117 @@
 #include <iostream>
 #include <array>
 #include <condition_variable>
-#include <queue>
+#include <chrono>
+
+class SharedMemory;
+
+// OS-agnostic semaphore abstraction
+class Semaphore
+{
+public:
+	enum class WaitResult : uint8_t
+	{
+		Signaled,
+		WaitAbandoned,
+		Timeout,
+		WaitFailed
+	};
+	Semaphore();
+	void Signal();
+	WaitResult Wait(std::chrono::duration<std::chrono::milliseconds> timeout);
+};
 
 // TODO: Move to a separate file and include platform specific things only in its implementation.
 class RoleCheck
 {
 public:
+	friend SharedMemory;
 	enum class Role : uint8_t
 	{
 		Reader,
 		Writer,
 		Exit // This process should exit
 	};
-	/// First creates an OS semaphore and check if this is first or second instance.
-	/// Then creates second OS semaphore if this is first (Reader) or second (Writer) instance.
 	RoleCheck();
-	Role GetRole();
+	std::pair<Role, SharedMemory> GetRole();
+private:
+	Semaphore GetSemaphore();
+};
+
+static constexpr std::streamsize BLOCK_SIZE = 1'000'000;
+static constexpr std::streamsize BLOCK_NUM = 3;
+
+struct Block {
+	char data[BLOCK_SIZE];
+	std::streamsize size;
 };
 
 class SharedMemory
 {
 public:
-	static constexpr std::streamsize BLOCK_SIZE = 1'000'000;
-	static constexpr std::streamsize BLOCK_NUM = 3;
-
-	struct Block {
-		char data[BLOCK_SIZE];
-		std::streamsize size;
-	};
-
-	struct GuardedQueue
-	{
-		std::queue<Block> queue;
-		std::mutex mutex;
-		std::condition_variable cv;
-	};
-
 	explicit SharedMemory(RoleCheck::Role role);
-	GuardedQueue GetEmptyBlocks();
-	GuardedQueue GetBlocksToWrite();
+
+	class DataTransfer {
+	public:
+		Block& GetBlock();
+		void PutBlock(Block& );
+	};
+
+	DataTransfer GetWriterInterface();
+	DataTransfer GetReaderInterface();
 
 private:
-	GuardedQueue emptyBlocks_;
-	GuardedQueue blocksToWrite_;
-	std::array<Block, BLOCK_NUM> blocks_; // used to fill EmptyBlocks at the program beginning.
+	//struct GuardedQueue
+	//{
+	//	std::queue<Block> queue;
+	//	std::mutex mutex;
+	//	std::condition_variable cv;
+	//};
+	//
+	//GuardedQueue GetEmptyBlocks();
+	//GuardedQueue GetBlocksToWrite();
+
+	//GuardedQueue emptyBlocks_;
+	//GuardedQueue blocksToWrite_;
+	//std::array<Block, BLOCK_NUM> blocks_; // used to fill EmptyBlocks at the program beginning.
 };
 
 int main()
 {
 	RoleCheck roleCheck; // Lifespan of this object is the whole process runtime.
-	RoleCheck::Role role = roleCheck.GetRole();
+	auto [role, sharedMemory] = roleCheck.GetRole();
 	if (role == RoleCheck::Role::Exit)
 	{
-		std::cout << "maximum of two projects exist.\n";
+		std::cout << "Maximum of two processes is allowed at same time. This one was decided to be third or more.\n";
 		return 0;
 	}
-	// Read command line parameters here.
-
-	SharedMemory sharedMemory(role);
+	// Read command-line parameters here.
 
 	if (role == RoleCheck::Role::Reader)
 	{
 		// Reader code here.
 		std::cout << "Reader process started.\n";
+		// open input file
+		auto rdMem = sharedMemory.GetReaderInterface();
+		while (true /*read not finished*/)
+		{
+			auto& block = rdMem.GetBlock();
+			// Read from file into emptyBlock.
+			rdMem.PutBlock(block);
+		}
 	}
 	else if (role == RoleCheck::Role::Writer)
 	{
 		// Writer code here.
 		std::cout << "Writer process started.\n";
+		auto wrMem = sharedMemory.GetWriterInterface();
+		bool finished = false;
+		do // last block has full size
+		{
+			auto& block = wrMem.GetBlock();
+			// Write to file from block.
+			wrMem.PutBlock(block);
+			finished = (block.size < BLOCK_SIZE);
+		} while (!finished /*write not finished*/);
 	}
 }
--- README.md
+++ README.md
@@ -1,30 +1,78 @@
 # Multiprocess communication
 
+Let's use one semaphore for both: Decidnig role and synchronizing shared memory creation.
+Let's prefer our API to not contein OS dependent object (like semaphore)
+
+## Decide process role
+
+Advantages: Clean interface for program.
+
+Disadvantages: Role knows SharedMemory.
+
+Maybe alternative: Role return OS-agnostic semaphore abstractions and program to give it to shared memory constructor together with role.
+
 ```mermaid
 sequenceDiagram
-    participant Shell
     participant Program
-    participant AppCount as AppCountSemaphore(start == max == 2)
-    participant IsReader as IsReaderSemaphore(start == max == 1)
-    participant MemoryBlock
-    activate AppCount
-    activate IsReader
-    Shell ->>+ Program: Run
-    Program ->>+ AppCount: Create
-    Program ->>+ IsReader: Create
-    Program ->> AppCount: WaitForSingleObject(0 ms) 
+    participant Role
+    participant AppCount as AppCount + IsReader Semaphore(start == 0, max == 1)
+    Participant SharedMemory
+    participant OS as OS(Windows)
+    Program ->> Role: GetRole
+    Role ->>+ AppCount: CreateSemaphoreA 
     alt
-        Program ->> IsReader: WaitForSingleObject(0 ms)
-        alt
-            IsReader ->> Program: Success
-            Program ->> MemoryBlock: Create(Name/UUID, BlockSize * Blocks)
-        else
-            IsReader ->> Program: Fail
-            Program ->> MemoryBlock: Get(Name/UUID)
-        end
+        AppCount ->> Role: Handle ( == This thread is reader.)
+        Role ->> SharedMemory: Create
+        SharedMemory ->> OS: CreateFileMapping(INVALID_HANDLE_VALUE...
+        OS ->> SharedMemory : Handle
+        SharedMemory ->> Role: SharedMemory&
+        Role ->> Program: Role(Reader, SharedMemory&)
+        Role ->> AppCount: ReleaseSemaphore
+        AppCount ->> Role: Non-zero / success
     else
-        Program ->>+ Shell : Too many instances
+        AppCount ->> Role: Handle + ERROR_ALREADY_EXISTS
+        Role ->> AppCount: WaitForSingleObject(0.5 second)
+        alt
+            AppCount ->> Role: Success == Signaled ( == This thread is writter)
+            Role ->> SharedMemory: GetExisting
+            SharedMemory ->> OS: OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, szName);
+            OS ->> SharedMemory: Handle
+            SharedMemory ->> Role: SharedMemory&
+            Role ->> Program: Role(Writer, SharedMemory&)
+        else
+            AppCount ->> Role: Timeout
+            Role ->>+ Program : Too many instances
+        end
     end
-    
-
 ```
+
+## Do copy
+```mermaid
+sequenceDiagram
+    participant Reader 
+    participant Writer
+    participant Mem as SharedMemory
+    participant RdMem as ReaderInterface
+    participant WtMem as WriterInterface    
+    par
+        Reader ->> Mem: GetReaderInterface
+        Mem ->> Reader: ReaderInterface&
+        loop
+            RdMem ->> Reader: Empty Block&
+            Reader ->> RdMem: Data Block& + Size
+        end
+    and
+        Writer ->> Mem: GetWritterInterface
+        Mem ->> Writer: WriterInterface&
+        loop
+            WtMem ->> Writer: Data Block& + Size
+            Writer ->> WtMem: Empty Block&
+        end
+    end
+```
+
+## Notes
+
+Shared memory in Windows: https://learn.microsoft.com/en-us/windows/win32/memory/creating-named-shared-memory
+
+Using semaphores in WIndows: https://learn.microsoft.com/en-us/windows/win32/sync/using-semaphore-objects?redirectedfrom=MSDN
